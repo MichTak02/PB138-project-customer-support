834c93c0d04bee92de2d5cb79612d52c9d3aa728
Ondrej Marek <xmarek9@fi.muni.cz>
2024-05-22 16:05:25 +0200

    feat: add user mapper

---
+++ backend/src/repositories/user/mappers.ts
@@ -0,0 +1,25 @@
+import {ChatCommunication, User, VoiceCommunication} from "@prisma/client";
+import {UserDto, UserExtendedDto} from "./types"
+
+export const userModelToUserDto = (userModel: User): UserDto => ({
+    id: userModel.id,
+    email: userModel.email,
+    displayName: userModel.displayName,
+    passwordHash: userModel.passwordHash,
+    createdOn: userModel.createdOn,
+    role: userModel.role
+});
+
+export const userModelToUserExtendedDto = (userModel: User & {
+    chatCommunications:    ChatCommunication[]
+    voiceCommunications:   VoiceCommunication[]
+}): UserExtendedDto => ({
+    id: userModel.id,
+    email: userModel.email,
+    displayName: userModel.displayName,
+    passwordHash: userModel.passwordHash,
+    createdOn: userModel.createdOn,
+    role: userModel.role,
+    chatCommunications: userModel.chatCommunications.map(cc => chatCommunicationModelToChatCommunicationDto(cc)),
+    voiceCommunications: userModel.voiceCommunications.map(vc => voiceCommunicationModelToVoiceCommunicationDto(vc)),
+});
