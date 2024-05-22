94b3b801a398a83e9fa2581ee08b520f20203ee8
Ondrej Marek <xmarek9@fi.muni.cz>
2024-05-22 17:36:56 +0200

    feat: add read to user repository

--- backend/src/repositories/user/userRepository.ts
+++ backend/src/repositories/user/userRepository.ts
@@ -1,9 +1,9 @@
 import {Result} from "@badrap/result";
 import {DbResult} from "../types";
-import {UserCreateDto, UserDto, UserUpdateDto} from "./types";
+import {UserCreateDto, UserDto, UserExtendedDto, UserFilters, UserUpdateDto} from "./types";
 import prisma from "../client";
-import {handleRepositoryErrors} from "../../utils/repositoryUtils";
-import {userModelToUserDto} from "./mappers";
+import {handleRepositoryErrors, READ_MANY_TAKE} from "../../utils/repositoryUtils";
+import {userModelToUserDto, userModelToUserExtendedDto} from "./mappers";
 
 export const userRepository = {
     async create (data: UserCreateDto): DbResult<UserDto> {
@@ -16,7 +16,7 @@ export const userRepository = {
         }
     },
 
-    async update(id: string, data: UserUpdateDto): DbResult<UserDto> {
+    async update(id: number, data: UserUpdateDto): DbResult<UserDto> {
         try {
             const updatedUser = await prisma.user.update({
                 data: data,
@@ -30,44 +30,13 @@ export const userRepository = {
         }
     },
 
-    async delete(id: string): DbResult<UserDto> {
+    async delete(id: number): DbResult<UserDto> {
         try {
-            const result = await prisma.$transaction(async (tx) => {
-                const userPlaylists = await tx.playlist.findMany({
-                    where: {
-                        ownerId: id
-                    },
-                    select: {id: true}
-                })
-
-
-                for (const userPlaylist of userPlaylists) {
-                    await tx.playlistSong.deleteMany({
-                        where: {
-                            playlistId: userPlaylist.id,
-                        },
-                    })
+            const result = await prisma.user.delete({
+                where : {
+                    id: id
                 }
-
-                await tx.playlist.deleteMany({
-                    where: {
-                        ownerId: id,
-                    },
-                })
-
-                await tx.userLikedSongs.deleteMany({
-                    where: {
-                        userId: id
-                    }
-                })
-
-                const deletedUser = await tx.user.delete({
-                    where: {
-                        id: id
-                    }
-                });
-                return deletedUser
-            }, {timeout: 10000});
+            })
 
             return Result.ok(userModelToUserDto(result));
         } catch (error) {
@@ -75,7 +44,14 @@ export const userRepository = {
         }
     },
 
-    async login(email: string, passwordHash: string): DbResult<string> {
+    /**
+     * Tries to authenticate the user by their email and password hash.
+     * @param email
+     * @param passwordHash
+     *
+     * @return User ID if successful
+     */
+    async login(email: string, passwordHash: string): DbResult<number> {
         try {
             const idObj = await prisma.user.findFirstOrThrow({
                 select: {
@@ -92,42 +68,46 @@ export const userRepository = {
         }
     },
 
-    async readOne(id: string): DbResult<UserExtended> {
+    async readOne(id: number): DbResult<UserDto> {
+        try {
+            const user = await prisma.user.findFirstOrThrow({
+                where : {
+                    id: id
+                }
+            });
+            return Result.ok(userModelToUserDto(user));
+        } catch (error) {
+            return handleRepositoryErrors(error);
+        }
+    },
+
+    async readOneExtended(id: number): DbResult<UserExtendedDto> {
         try {
             const user = await prisma.user.findFirstOrThrow({
                 where : {
                     id: id
                 },
                 include: {
-                    playlists: true,
-                    albums: true,
-                    likedSongs: {select: {song: true, likedOn: true}},
-                    lastPlayedSong: true
+                    chatCommunications: true,
+                    voiceCommunications: true,
                 }
             });
-            return Result.ok(dbUserToUserExtended(user));
+            return Result.ok(userModelToUserExtendedDto(user));
         } catch (error) {
             return handleRepositoryErrors(error);
         }
     },
 
-    async readMany(cursorId: string | undefined): DbResult<UserExtended[]> {
+    async readMany(cursorId: number | undefined, filter?: UserFilters): DbResult<UserDto[]> {
         try {
-            const includeObj = {
-                playlists: true,
-                albums: true,
-                likedSongs: {select: {song: true, likedOn: true}},
-                lastPlayedSong: true
-            }
-
             if (!cursorId) {
                 const users = await prisma.user.findMany({
                     take: READ_MANY_TAKE,
                     orderBy: { id: 'asc' },
-                    include: includeObj
+                    where: filter
                 });
 
-                return Result.ok(users.map(u => dbUserToUserExtended(u)));
+                return Result.ok(users.map(u => userModelToUserDto(u)));
             }
 
             const users = await prisma.user.findMany({
@@ -135,140 +115,47 @@ export const userRepository = {
                 skip: 1,
                 cursor: { id: cursorId },
                 orderBy: { id: 'asc' },
-                include: includeObj
+                where: filter
             });
-            return Result.ok(users.map(u => dbUserToUserExtended(u)));
+            return Result.ok(users.map(u => userModelToUserDto(u)));
         } catch (error) {
             return handleRepositoryErrors(error);
         }
     },
 
-    async likeSong(userId: string, songId: string): DbResult<string> {
+    async readManyExtended(cursorId: number | undefined, filter?: UserFilters): DbResult<UserExtendedDto[]> {
         try {
-            await prisma.userLikedSongs.create({
-                data: {
-                    likedOn: new Date(Date.now()),
-                    user: {connect: {id: userId}},
-                    song: {connect: {id: songId}},
-                },
+            const includeObj = {
+                chatCommunications: true,
+                voiceCommunications: true,
+            }
+
+            if (!cursorId) {
+                const users = await prisma.user.findMany({
+                    take: READ_MANY_TAKE,
+                    orderBy: { id: 'asc' },
+                    include: includeObj,
+                    where: filter
+                });
+
+                return Result.ok(users.map(u => userModelToUserExtendedDto(u)));
+            }
+
+            const users = await prisma.user.findMany({
+                take: READ_MANY_TAKE,
+                skip: 1,
+                cursor: { id: cursorId },
+                orderBy: { id: 'asc' },
+                include: includeObj,
+                where: filter
             });
-            return Result.ok(songId);
+            return Result.ok(users.map(u => userModelToUserExtendedDto(u)));
         } catch (error) {
             return handleRepositoryErrors(error);
         }
     },
 
-    async unlikeSong(userId: string, songId: string): DbResult<string> {
-        try {
-            await prisma.userLikedSongs.delete({
-                where: {
-                    userLikedSongsId: {
-                        userId: userId,
-                        songId: songId
-                    }
-                },
-            });
-            return Result.ok(songId);
-        } catch (error) {
-            return handleRepositoryErrors(error);
-        }
-    },
 
-    async setLastPlayedSong(userId: string, songId: string): DbResult<string> {
-        try {
-            await prisma.user.update({
-                where: {
-                    id: userId
-                },
-                data: {
-                    lastPlayedSong: {
-                        connect: {
-                            id: songId
-                        }
-                    }
-                }
-            });
-            return Result.ok(songId);
-        } catch (error) {
-            return handleRepositoryErrors(error);
-        }
-    },
-
-    async addPlaylist(userId: string, playlistId: string): DbResult<string> {
-        try {
-            await prisma.user.update({
-                where: {
-                    id: userId
-                },
-                data: {
-                    playlists: {
-                        connect: {id: playlistId}
-                    }
-                }
-            })
-
-            return Result.ok(playlistId);
-        } catch (error) {
-            return handleRepositoryErrors(error);
-        }
-    },
-
-    async removePlaylist(userId: string, playlistId: string): DbResult<string> {
-        try {
-            await prisma.user.update({
-                where: {
-                    id: userId
-                },
-                data: {
-                    playlists: {
-                        disconnect: {id: playlistId}
-                    }
-                }
-            })
-
-            return Result.ok(playlistId);
-        } catch (error) {
-            return handleRepositoryErrors(error);
-        }
-    },
-
-    async addAlbum(userId: string, albumId: string): DbResult<string> {
-        try {
-            await prisma.user.update({
-                where: {
-                    id: userId
-                },
-                data: {
-                    albums: {
-                        connect: {id: albumId}
-                    }
-                }
-            })
-
-            return Result.ok(albumId);
-        } catch (error) {
-            return handleRepositoryErrors(error);
-        }
-    },
-
-    async removeAlbum(userId: string, albumId: string): DbResult<string> {
-        try {
-            await prisma.user.update({
-                where: {
-                    id: userId
-                },
-                data: {
-                    albums: {
-                        disconnect: {id: albumId}
-                    }
-                }
-            })
-
-            return Result.ok(albumId);
-        } catch (error) {
-            return handleRepositoryErrors(error);
-        }
-    }
 }
 
 export default userRepository;
