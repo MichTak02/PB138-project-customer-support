b860491be0248ff5673dea27b28a6cab331ef113
Martin Štalmach <xstalmac@fi.muni.cz>
2024-05-22 19:51:05 +0200

    feat: create category validation schemas

---
+++ backend/src/validationSchemas/cateogryValidationSchemas.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+export const createCategorySchema = z.object({
+  body: z.object({
+    name: z.string().min(1).max(255),
+  }),
+});
+
+export const getCategorySchema = z.object({
+  params: z.object({
+      id: z.coerce.number(),
+    }),
+  });
+
+
+export const getCategoriesSchema = z.object({
+  query: z.object({
+    page: z.coerce.number().optional(),
+    name: z.string().optional(),
+  }),
+});
+
+export const updateCategorySchema = z.object({
+  params: z.object({
+    id: z.coerce.number(),
+  }),
+  body: z.object({
+    name: z.string().min(1).max(255),
+  }),
+});
+
+export const deleteCategorySchema = z.object({
+  params: z.object({
+    id: z.coerce.number(),
+  }),
+});
