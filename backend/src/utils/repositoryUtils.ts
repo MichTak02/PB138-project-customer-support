7cfd394b75ee2b98056406bd23f9f434b210d421
Ondrej Marek <xmarek9@fi.muni.cz>
2024-05-22 10:06:35 +0200

    feat: introduce utils classes

---
+++ backend/src/utils/repositoryUtils.ts
@@ -0,0 +1,16 @@
+import {Result} from "@badrap/result";
+import { Prisma } from "@prisma/client";
+import {ConflictError, InternalError, NotFoundError} from "../repositories/errors";
+
+export const handleRepositoryErrors = (e: unknown) => {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+            return Result.err(new ConflictError(e.message));
+        }
+        if (e.code === 'P2001') {
+            return Result.err(new NotFoundError(e.message));
+        }
+    }
+
+    return Result.err(new InternalError());
+}
