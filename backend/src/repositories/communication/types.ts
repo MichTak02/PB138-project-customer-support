6be4fa4cc998019c664629c0552f7634f7be278c
Ondrej Marek <xmarek9@fi.muni.cz>
2024-05-22 22:59:08 +0200

    feat: add voice and chat extended dtos

--- backend/src/repositories/communication/types.ts
+++ backend/src/repositories/communication/types.ts
@@ -1,34 +1,42 @@
 import {BaseModelId} from "../types";
 import {UserDto} from "../user/types";
+import {CustomerDto} from "../customer/types";
 
 export type ChatCommunicationDto = BaseModelId & {
     message:                string
     timestamp:              Date
     isUserSent:             boolean
-    user:                   UserDto
-    customer:               CustomerDto
-}
-
-export type ChatCommunicationCreateDto = Omit<ChatCommunicationDto, "id" | "user" | "customer">
+};
+export type ChatCommunicationCreateDto = Omit<ChatCommunicationDto, "id">
     & {
     customerId: number
     userId: number
 };
 export type ChatCommunicationUpdateDto = Partial<ChatCommunicationCreateDto>
+export type ChatCommunicationExtendedDto = ChatCommunicationDto & {
+    user:                   UserDto
+    customer:               CustomerDto
+}
+export type ChatCommunicationFilters = Partial<ChatCommunicationCreateDto>
+
+
 
 export type VoiceCommunicationDto = BaseModelId & {
     filePath:               string
     start:                  Date
     end:                    Date
     isUserStarted:          boolean
-    user:                   UserDto
-    customer:               CustomerDto
 }
 
-export type VoiceCommunicationCreateDto = Omit<VoiceCommunicationDto, "id" | "user" | "customer">
+export type VoiceCommunicationCreateDto = Omit<VoiceCommunicationDto, "id">
     & {
     customerId: number
     userId: number
 };
 
 export type VoiceCommunicationUpdateDto = Partial<VoiceCommunicationCreateDto>
+export type VoiceCommunicationExtendedDto = VoiceCommunicationDto & {
+    user:                   UserDto
+    customer:               CustomerDto
+}
+export type VoiceCommunicationFilters = Partial<VoiceCommunicationCreateDto>
