77de742f368f631535c177fe84216325f3b221e1
Ondrej Marek <xmarek9@fi.muni.cz>
2024-06-05 21:05:42 +0200

    fix: separate offerToProduct and offer types

---
+++ backend/src/repositories/offerToProduct/types.ts
@@ -0,0 +1,18 @@
+import {BaseModelId} from "../types";
+import {ProductExtendedDto} from "../product/types";
+
+export type OfferToProductDto = BaseModelId & {
+    offerId: number,
+    productId: number,
+    productQuantity: number,
+    newPrice: number
+}
+
+export type OfferToProductCreateDto = Omit<OfferToProductDto, "id">
+export type OfferToProductUpdateDto = Partial<OfferToProductCreateDto>
+
+export type OfferToProductExtendedDto = Omit<OfferToProductDto, "productId"> & {
+    product: ProductExtendedDto,
+}
+
+export type OfferToProductFilter = Partial<OfferToProductCreateDto>
