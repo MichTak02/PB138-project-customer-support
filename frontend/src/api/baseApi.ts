99baafd8bb68849feaf835378538ef7e137f08e0
Ondrej Marek <xmarek9@fi.muni.cz>
2024-06-05 22:40:59 +0200

    feat: add get single entity to frontend api

--- frontend/src/api/baseApi.ts
+++ frontend/src/api/baseApi.ts
@@ -9,6 +9,11 @@ async function getAll<T>(path: string, config?: AxiosRequestConfig): Promise<T> 
   return response.data;
 }
 
+async function get<T>(path: string) {
+  const response = await axiosInstance.get<T>(path);
+  return response.data;
+}
+
 async function postSingle<T>(path: string, payload: unknown): Promise<T> {
   const response = await axiosInstance.post<T>(path, payload);
   return response.data;
@@ -26,6 +31,7 @@ async function deleteSingle<T>(path: string): Promise<T> {
 
 const BaseApi = {
   getAll,
+  get,
   postSingle,
   putSingle,
   deleteSingle,
