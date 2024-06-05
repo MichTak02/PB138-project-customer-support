042171abd53c7ba37b571501772588bc7cf46d86
Ondrej Marek <xmarek9@fi.muni.cz>
2024-06-05 21:18:59 +0200

    fix: adjust offer mappers due to type changes & repository separation

--- backend/src/repositories/offer/mappers.ts
+++ backend/src/repositories/offer/mappers.ts
@@ -1,28 +1,15 @@
-import { Offer, OfferToProduct } from "@prisma/client";
-import { OfferDto, OfferExtendedDto, OfferToProductDto, OfferToProductExtendedDto, OfferToProductWithProduct, OfferWithOfferToProductWithProducts, OfferWithOfferToProducts } from "./types";
-import { productModelToProductExtendedDto } from "../product/mappers";
+import {Offer} from "@prisma/client";
+import { OfferDto, OfferExtendedDto } from "./types";
+import {offerToProductModelToOfferToProductExtendedDto, OfferToProductWithProduct} from "../offerToProduct/mappers";
 
-export const offerToProductModelToOfferToProductDto = (model: OfferToProduct): OfferToProductDto => ({
-    id: model.id,
-    offerId: model.offerId,
-    productId: model.productId,
-    newPrice: model.newPrice,
-    productQuantity: model.productQuantity
-})
+export type OfferWithOfferToProductWithProducts = Offer & {
+    offerToProducts: OfferToProductWithProduct[],
+}
 
-export const offerToProductModelToOfferToProductExtendedDto = (model: OfferToProductWithProduct): OfferToProductExtendedDto => ({
-    id: model.id,
-    offerId: model.offerId,
-    product: productModelToProductExtendedDto(model.product),
-    newPrice: model.newPrice,
-    productQuantity: model.productQuantity,
-})
-
-export const offerModelToOfferDto = (model: OfferWithOfferToProducts): OfferDto => ({
+export const offerModelToOfferDto = (model: Offer): OfferDto => ({
     id: model.id,
     description: model.description,
     name: model.name,
-    offerToProducts: model.offerToProducts.map((otp) => offerToProductModelToOfferToProductDto(otp)),
 })
 
 export const offerModelToOfferExtendedDto = (model: OfferWithOfferToProductWithProducts): OfferExtendedDto => ({
