2eddf3eb672d5e8b3d68e26cd33f1bafb51d7154
Ondrej Marek <xmarek9@fi.muni.cz>
2024-06-07 01:01:59 +0200

    feat: add customer types

--- frontend/src/models/customer.ts
+++ frontend/src/models/customer.ts
@@ -1,10 +1,21 @@
-export type Customer = {
-    id: number;
-    name: string;
-    surname: string;
-    email: string;
-    phoneNumber: string;
-  };
-  
-  export type CustomerBasic = Omit<Customer, 'id'>;
+import {BaseModelId} from "./base.ts";
+import {ProductDto} from "./product.ts";
+import {ChatCommunicationDto} from "./chatCommunication.ts";
+import {VoiceCommunicationDto} from "./voiceCommunication.ts";
+
+export type CustomerDto = BaseModelId & {
+    name:     string
+    surname:     string
+    email:        string
+    phoneNumber: string
+}
+
+export type CustomerCreateDto = Omit<CustomerDto, "id"> & { productIds: number[] };
+export type CustomerUpdateDto = Partial<CustomerCreateDto>
+export type CustomerExtendedDto = CustomerDto & {
+    products:            ProductDto[]
+    chatCommunications:  ChatCommunicationDto[]
+    voiceCommunications: VoiceCommunicationDto[]
+}
+export type CustomerFilters = Partial<CustomerCreateDto>
   
