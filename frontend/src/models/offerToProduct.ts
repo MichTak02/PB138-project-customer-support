683152a9b994191a50d59ec70f3d1f12afc509ae
Ondrej Marek <xmarek9@fi.muni.cz>
2024-06-07 00:57:45 +0200

    feat: add offerToProduct types

---
+++ frontend/src/models/offerToProduct.ts
@@ -0,0 +1,19 @@
+import {BaseModelId} from "./base.ts";
+import {ProductExtendedDto} from "./product.ts";
+
+
+export type OfferToProductDto = BaseModelId & {
+    offerId: number,
+    productId: number,
+    productQuantity: number,
+    newPrice: number
+}
+
+export type OfferToProductCreateDto = Omit<OfferToProductDto, "id">
+export type OfferToProductUpdateDto = Partial<OfferToProductCreateDto>
+
+export type OfferToProductExtendedDto = Omit<OfferToProductDto, "productId"> & {
+    product: ProductExtendedDto,
+}
+
+export type OfferToProductFilter = Partial<OfferToProductCreateDto>
