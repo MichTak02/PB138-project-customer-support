ade52c2de28d31194ea0250658d38031ab580ae8
Ondrej Marek <xmarek9@fi.muni.cz>
2024-06-07 01:00:49 +0200

    feat: restructure categories hooks

--- frontend/src/hooks/useCategories.tsx
+++ frontend/src/hooks/useCategories.tsx
@@ -1,49 +1,63 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import CategoriesApi from '../api/categoryApi';
-import { CategoryDto, CategoryCreateDto, CategoryUpdateDto, CategoryFilters } from '../models/category';
+import {useQuery, useMutation, useQueryClient, useInfiniteQuery} from '@tanstack/react-query';
+import {CategoryDto, CategoryCreateDto, CategoryUpdateDto, CategoryFilters} from '../models/category';
+import ApiService, {GET_MANY_SIZE} from "../api/apiService.ts";
 
-export const useCategories = (cursorId?: number, filter?: CategoryFilters) => {
-  return useQuery<CategoryDto[]>({
-    queryKey: ['categories', cursorId, filter],
-    queryFn: () => CategoriesApi.getAllCategories(cursorId, filter),
-  });
+const categoriesApi = new ApiService<CategoryDto, CategoryDto, CategoryCreateDto, CategoryUpdateDto, CategoryFilters>('/categories');
+
+export const useCategories = (filter?: CategoryFilters) => {
+    const {
+        data,
+        error,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+        isFetchingNextPage,
+        status,
+    } = useInfiniteQuery({
+        queryKey: ['categories'],
+        queryFn: ({pageParam}) => categoriesApi.getMany(pageParam, filter),
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, _pages) => lastPage.length === GET_MANY_SIZE ? lastPage[lastPage.length - 1].id : undefined,
+    });
+
+    return {data, error, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, status}
 };
 
 export const useCategory = (id: number) => {
-  return useQuery<CategoryDto>({
-    queryKey: ['category', id],
-    queryFn: () => CategoriesApi.getCategory(id),
-  });
+    return useQuery<CategoryDto>({
+        queryKey: ['category', id],
+        queryFn: () => categoriesApi.get(id),
+    });
 };
 
 export const useCreateCategory = () => {
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationFn: CategoriesApi.createCategory,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['categories'] });
-    },
-  });
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: categoriesApi.create,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['categories']});
+        },
+    });
 };
 
-export const useUpdateCategory = () => {
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationFn: ({ id, category }: { id: number; category: CategoryUpdateDto }) =>
-      CategoriesApi.updateCategory(id, category),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['categories'] });
-    },
-  });
+export const useUpdateCategory = (id: number) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (categoryData: CategoryUpdateDto) =>
+            categoriesApi.update(id, categoryData),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['categories']});
+        },
+    });
 };
 
 
-export const useDeleteCategory = () => {
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationFn: (id: number) => CategoriesApi.deleteCategory(id),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['categories'] });
-    },
-  });
+export const useDeleteCategory = (id: number) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: () => categoriesApi.delete(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['categories']});
+        },
+    });
 };
