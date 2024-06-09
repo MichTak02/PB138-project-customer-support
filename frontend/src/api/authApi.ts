a925498f789da735711cd1144c9abb89ca299553
Ondrej Marek <xmarek9@fi.muni.cz>
2024-06-09 16:16:07 +0200

    feat: add register api function

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,12 +1,17 @@
 import BaseApi from "./baseApi.ts";
-import {AuthDto, LoginDto} from "../models/auth.ts";
+import {AuthDto, LoginDto, RegisterDto} from "../models/auth.ts";
 
 const LOGIN_PREFIX = '/auth/login';
 const LOGOUT_PREFIX = '/auth/logout';
+const REGISTER_PREFIX = '/auth/register';
 const AUTH_PREFIX = '/auth';
 
 async function login(auth: LoginDto) {
     return BaseApi.postSingle<AuthDto>(LOGIN_PREFIX, auth);
+}
+
+async function register(registerData: RegisterDto) {
+    return BaseApi.postSingle<void>(REGISTER_PREFIX, registerData);
 }
 
 async function auth() {
@@ -20,7 +25,8 @@ async function logout() {
 const AuthApi = {
     login,
     auth,
-    logout
+    logout,
+    register
 };
 
 export default AuthApi;
