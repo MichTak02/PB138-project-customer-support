4981bf054bc4a326f746f94537e3a07740912ff5
Martin Štalmach <xstalmac@fi.muni.cz>
2024-06-10 17:43:55 +0200

    feat: add icons to dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,30 +1,43 @@
 import Page from '../components/base/Page';
-import { Typography, List, ListItem, ListItemText, Divider } from '@mui/material';
+import { Typography, List, ListItem, ListItemText, Divider, ListItemIcon } from '@mui/material';
 import { Link } from 'react-router-dom';
+import PeopleIcon from '@mui/icons-material/People';
+import PersonIcon from '@mui/icons-material/Person';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import CategoryIcon from '@mui/icons-material/Category';
+import PercentIcon from '@mui/icons-material/Percent';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 
 export function Dashboard() {
+  const items = [
+    { text: "User Management", link: "/auth/users", icon: <PeopleIcon /> },
+    { text: "Customer Management", link: "/auth/customers", icon: <PersonIcon /> },
+    { text: "Product Management", link: "/auth/products", icon: <InventoryIcon /> },
+    { text: "Category Management", link: "/auth/categories", icon: <CategoryIcon /> },
+    { text: "Offer Management", link: "/auth/offers", icon: <PercentIcon /> },
+  ];
+
+  const communications = [
+    { text: "Voice Communications", link: "/customers/:customerId/voice", icon: <KeyboardVoiceIcon /> },
+    { text: "Chat Communications", link: "/customers/:customerId/chat", icon: <ChatBubbleIcon /> },
+  ];
+
   return (
     <Page title="Dashboard">
       <Typography component="h2" variant="h5" gutterBottom>
         Management
       </Typography>
       <List>
-        <ListItem button component={Link} to="/users">
-          <ListItemText primary="User Management" />
+      {items.map((item, index) => (
+        <ListItem button component={Link} to={item.link} key={index}>
+          <ListItemIcon>
+            {item.icon}
+          </ListItemIcon>
+          <ListItemText primary={item.text} />
         </ListItem>
-        <ListItem button component={Link} to="/customers">
-          <ListItemText primary="Customer Management" />
-        </ListItem>
-        <ListItem button component={Link} to="/products">
-          <ListItemText primary="Product Management" />
-        </ListItem>
-        <ListItem button component={Link} to="/categories">
-          <ListItemText primary="Category Management" />
-        </ListItem>
-        <ListItem button component={Link} to="/offers">
-          <ListItemText primary="Offer Management" />
-        </ListItem>
-      </List>
+      ))}
+    </List>
       
       <Divider sx={{ my: 2 }} />
 
@@ -32,13 +45,15 @@ export function Dashboard() {
         Communications
       </Typography>
       <List>
-        <ListItem button component={Link} to="/customers/:customerId/voice">
-          <ListItemText primary="Voice Communications" />
+      {communications.map((communication, index) => (
+        <ListItem button component={Link} to={communication.link} key={index}>
+          <ListItemIcon>
+            {communication.icon}
+          </ListItemIcon>
+          <ListItemText primary={communication.text} />
         </ListItem>
-        <ListItem button component={Link} to="/customers/:customerId/chat">
-          <ListItemText primary="Chat Communications" />
-        </ListItem>
-      </List>
+      ))}
+    </List>
     </Page>
   );
 }
