5c92067fdd6557153a01950240194e3085b28808
Michal Takac <xtakac@fi.muni.cz>
2024-06-10 20:38:17 +0200

    fix: fixed date rendering in chat message

--- frontend/src/components/Communication/ChatMessage/ChatMessage.tsx
+++ frontend/src/components/Communication/ChatMessage/ChatMessage.tsx
@@ -1,22 +1,17 @@
 import "./chat-message.css"
 import "../../../index.css"
+import {ChatCommunicationDto} from "../../../models/chatCommunication.ts";
 
-interface Message {
-    text: string;
-    createdAt: Date;
-    isOutgoing: boolean;
-}
-
-export const ChatMessage = (props: { message: Message }) => {
-    const date = props.message.createdAt;
-    const messageClass: string = props.message.isOutgoing ? "chat-message chat-message--sent" : "chat-message chat-message--received";
-    const messageTextClass: string = props.message.isOutgoing ? "chat-message-text chat-message-text--sent" : "chat-message-text chat-message-text--received";
+export const ChatMessage = (props: { message: ChatCommunicationDto }) => {
+    const date = new Date(props.message.timestamp).toLocaleString();
+    const messageClass: string = props.message.isUserSent ? "chat-message chat-message--sent" : "chat-message chat-message--received";
+    const messageTextClass: string = props.message.isUserSent ? "chat-message-text chat-message-text--sent" : "chat-message-text chat-message-text--received";
 
     return (
         <div className={messageClass}>
-            <div className="chat-message-date">{date.toLocaleString()}</div>
+            <div className="chat-message-date">{date}</div>
             <div className={messageTextClass}>
-                {props.message.text}
+                {props.message.message}
             </div>
         </div>
     );
