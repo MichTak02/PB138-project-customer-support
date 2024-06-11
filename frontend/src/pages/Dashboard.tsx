0c21d6decd063450a764cc8ed9fed7db69304367
Ondrej Marek <xmarek9@fi.muni.cz>
2024-06-11 09:03:12 +0200

    feat: authz for User managment

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -6,14 +6,16 @@ import customerIcon from '../assets/customer-icon.svg'
 import productIcon from '../assets/product-icon.svg'
 import categoryIcon from '../assets/category-icon.svg'
 import offerIcon from '../assets/offer-icon.svg'
+import {RoleValues} from "../models/user.ts";
+import Authorized from "../components/auth/Authorized.tsx";
 
 export function Dashboard() {
   const items = [
-    { text: 'User Management', link: '/auth/users', image: userIcon },
-    { text: 'Customer Management', link: '/auth/customers', image: customerIcon },
-    { text: 'Product Management', link: '/auth/products', image: productIcon },
-    { text: 'Category Management', link: '/auth/categories', image: categoryIcon },
-    { text: 'Offer Management', link: '/auth/offers', image: offerIcon },
+    { text: 'User Management', link: '/auth/users', image: userIcon, role: RoleValues.ADMIN },
+    { text: 'Customer Management', link: '/auth/customers', image: customerIcon, role: RoleValues.REGULAR },
+    { text: 'Product Management', link: '/auth/products', image: productIcon, role: RoleValues.REGULAR },
+    { text: 'Category Management', link: '/auth/categories', image: categoryIcon, role: RoleValues.REGULAR },
+    { text: 'Offer Management', link: '/auth/offers', image: offerIcon, role: RoleValues.REGULAR },
   ];
 
   return (
@@ -23,7 +25,8 @@ export function Dashboard() {
       </Typography>
       <List style={{ display: "flex", flexWrap: "wrap", gap: "2rem" }}>
       {items.map((item, index) => (
-        <Card key={index} component={Link} to={item.link} style={{ textDecoration: "none", width: "20rem" }}>
+          <Authorized role={item.role} key={index}>
+        <Card component={Link} to={item.link} style={{ textDecoration: "none", width: "20rem" }}>
         <CardMedia
           component="div"
           style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "10rem" , padding: "1rem 0rem 0rem 0rem"}}
@@ -36,6 +39,7 @@ export function Dashboard() {
           </Typography>
         </CardContent>
       </Card>
+          </Authorized>
       ))}
     </List>
     </Page>
